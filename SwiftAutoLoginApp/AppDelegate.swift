1499b504849ebaa6010b6647f90f1982c167dfbb
ThanhNH <thanhnh@vitalify.asia>
2018-12-03 11:49:44 +0700

    update to swift 4.2

--- SwiftAutoLoginApp/AppDelegate.swift
+++ SwiftAutoLoginApp/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let applicationkey = "YOUR_NCMB_APPLICATIONKEY"
     let clientkey      = "YOUR_NCMB_CLIENTKEY"
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // SDKの初期化
         NCMB.setApplicationKey(applicationkey, clientKey: clientkey)
         
